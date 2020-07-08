b5e6ff92857077137388501202aff606621bf85f
KatieCollier <katie.collier@hotmail.fr>
2020-07-08 16:37:01 +0200

    modify sauce

--- So_Pekocko/backend/controllers/sauce.js
+++ So_Pekocko/backend/controllers/sauce.js
@@ -29,11 +29,18 @@ exports.getOneSauce = (req, res, next) => {
         .catch(error => res.status(404).json({error}));
 };
 
-/*
 exports.modifySauce = (req, res, next) => {
-
+    const sauceObject = req.file?
+    {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
+    }: {...req.body};
+    Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
+    .then(() => res.status(200).json({message: "Sauce mise à jour"}))
+    .catch(error => res.status(400).json({error}));
 };
 
+/*
 exports.deletSauce = (req, res, next) => {
 
 };*/
--- So_Pekocko/backend/routes/sauce.js
+++ So_Pekocko/backend/routes/sauce.js
@@ -6,6 +6,7 @@ const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 
 router.post("/", auth, multer, sauceCtrl.createSauce);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.get("/", auth, sauceCtrl.getSauces);
 
