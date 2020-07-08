6dd3c3b5df95aaa74b9f902593a99264620a5d41
KatieCollier <katie.collier@hotmail.fr>
2020-07-08 16:43:19 +0200

    delete sauce

--- So_Pekocko/backend/controllers/sauce.js
+++ So_Pekocko/backend/controllers/sauce.js
@@ -40,7 +40,16 @@ exports.modifySauce = (req, res, next) => {
     .catch(error => res.status(400).json({error}));
 };
 
-/*
-exports.deletSauce = (req, res, next) => {
 
-};*/
+exports.deletSauce = (req, res, next) => {
+    Sauce.findOne({_id: req.params.id})
+        .then(sauce => {
+            const filename = sauce.imageUrl.split("/images/")[1];
+            fs.unlink(`images/${filename}`, () => {
+                Sauce.deleteOne({_id: req.params.id})
+                    .then(() => res.status(200).json({message: "Sauce supprimée"}))
+                    .catch(error => res.status(400).json({error}));
+            })
+        })
+        .catch(error => res.status(500).json({error}));
+};
